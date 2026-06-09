2c1b22f87e6eed5a2cc6128172e553e340fc1f68
mrsangsuhome-web <mrsangsuhome@gmail.com>
2026-06-09 22:08:55 +0700

    fix ads banner props

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,1 +1,28 @@
-export default function AdBanner(){return <div>Ad Banner</div>}
+
+"use client";
+
+type AdBannerProps = {
+  slot?: string;
+};
+
+export default function AdBanner({
+  slot,
+}: AdBannerProps) {
+
+  return (
+
+    <div className="border rounded-xl p-6 text-center my-6 bg-gray-50">
+
+      <div className="text-sm text-gray-500">
+        Google AdSense
+      </div>
+
+      <div className="font-semibold">
+        Ad Slot: {slot || "default"}
+      </div>
+
+    </div>
+
+  );
+}
+
